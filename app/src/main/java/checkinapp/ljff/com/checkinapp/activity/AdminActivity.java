1b1ddc6491ccf20e3b3aba24c41f5540baf8e01c
phouthasak douanglee <jphouthasakdouanglee@gmail.com>
2018-03-18 01:09:57 -0400

    Added Admin ui events.

--- app/src/main/java/checkinapp/ljff/com/checkinapp/activity/AdminActivity.java
+++ app/src/main/java/checkinapp/ljff/com/checkinapp/activity/AdminActivity.java
@@ -1,7 +1,9 @@
 package checkinapp.ljff.com.checkinapp.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -21,5 +23,47 @@ public class AdminActivity extends AppCompatActivity{
     public void goToMainActivity(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void pushData(View view){
+        DialogInterface.OnClickListener pushDataDialogClickListener = new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        //Push the data here
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        builder.setMessage(R.string.pushDataWarning)
+                .setPositiveButton("Yes", pushDataDialogClickListener)
+                .setNegativeButton("No", pushDataDialogClickListener)
+                .show();
+    }
+
+    public void pullData(View view){
+        DialogInterface.OnClickListener pullDataDialogClickListener = new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        //Pull the data here
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        builder.setMessage(R.string.pullDataWarning)
+                .setPositiveButton("Yes", pullDataDialogClickListener)
+                .setNegativeButton("No", pullDataDialogClickListener)
+                .show();
     }
 }
--- app/src/main/java/checkinapp/ljff/com/checkinapp/fragment/CameraFragment.java
+++ app/src/main/java/checkinapp/ljff/com/checkinapp/fragment/CameraFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,35 +27,41 @@ public class CameraFragment extends Fragment implements ZXingScannerView.ResultH
         super.onCreate(savedInBundle);
         scannerView = new ZXingScannerView(getActivity());
         scannerView.setResultHandler(this);
+        Log.w("Test", "In OnCreate");
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.w("Test", "In OnCreateView");
         return scannerView;
     }
 
     @Override
     public void onStart(){
+        Log.w("Test", "In OnStart");
         super.onStart();
         scannerView.startCamera();
     }
 
     @Override
     public void onResume(){
+        Log.w("Test", "In OnResume");
         super.onResume();
         scannerView.resumeCameraPreview(this);
     }
 
     @Override
     public void onPause(){
+        Log.w("Test", "In OnPause");
         super.onPause();
         scannerView.stopCamera();
     }
 
     @Override
     public void handleResult(Result result) {
+        Log.w("Test", "In HandleResult");
         String studentId = result.getText();
         scannerView.stopCamera();
         Intent intent = new Intent(getActivity(), ProfileActivity.class);
